e47051869118bb230d50f0a8caeb560ddb315146
robel desta <rmdesta52@gmail.com>
2025-12-05 18:25:59 +0100

    ...

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel,
+                             QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.city_name = QLabel('Enter city name:', self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton('Get Weather', self)
+        self.temperature_label = QLabel("35°C", self)
+        self.emoji_label = QLabel("☀️", self)
+        self.description_label = QLabel("Sunny", self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Weather App')
+
+        vbox = QVBoxLayout()
+
+        vbox.addWidget(self.city_name)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        self.city_name.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
